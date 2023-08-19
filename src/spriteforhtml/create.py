f4d8ba16de7cd30efc3924610dc66705451a238c
Pascal Brand <pascal.brand38@gmail.com>
2023-08-19 18:18:34 +0200

    New format for the json - better readability
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

--- src/spriteforhtml/create.py
+++ src/spriteforhtml/create.py
@@ -11,6 +11,7 @@ import shutil
 
 
 # TODO: check the sprite does not overlap in some icons
+# TODO: auto-position the sprite
 
 def create_sprites(spriteJsonFilename):
   try:
@@ -24,18 +25,17 @@ def create_sprites(spriteJsonFilename):
   cssAllClasses = ''
 
   rootDirIcons = os.path.dirname(spriteJsonFilename)
-  iconsKeys = json_db['icons'].keys()
+  subimages = json_db['subimages']
 
   sprite_width = 0
   sprite_height = 0
   images = []
 
-  for iconKey in iconsKeys:
-    desc = json_db['icons'][iconKey]
-    name = getFullFilename(desc['filename'], rootDirIcons)
+  for subimage in subimages:
+    name = getFullFilename(subimage['filename'], rootDirIcons)
 
-    pos_w = int(desc['posHor'])
-    pos_h = int(desc['posVer'])
+    pos_w = int(subimage['posHor'])
+    pos_h = int(subimage['posVer'])
     i = Image.open(name)
     if sprite_width < pos_w + i.width:
       sprite_width = pos_w + i.width
@@ -43,50 +43,42 @@ def create_sprites(spriteJsonFilename):
       sprite_height = pos_h + i.height
     images.append(i)
 
-  # if sprite_width % 8 != 0:
-  #   sprite_width = math.floor(sprite_width / 8) * 8 + 8
-  # if sprite_height % 8 != 0:
-  #   sprite_height = math.floor(sprite_height / 8) * 8 + 8
-    
   sprite = Image.new(
     mode='RGBA',
     size=(sprite_width, sprite_height),
     color=(0,0,0,0))  # fully transparent
 
   index = 0
-  for iconKey in iconsKeys:
+  for subimage in subimages:
     i = images[index]
-
-    desc = json_db['icons'][iconKey]
-    name = desc['filename']
-    pos_w = int(desc['posHor'])
-    pos_h = int(desc['posVer'])
+    pos_w = int(subimage['posHor'])
+    pos_h = int(subimage['posVer'])
 
     sprite.paste(i, (pos_w, pos_h))
-    pseudo = desc.get('pseudo', '')
+    pseudo = subimage.get('cssPseudo', '')
 
-    cssString += iconKey + pseudo + ' {'                                                \
-      + ' background-position: -' + str(desc['posHor']) + 'px -' + str(desc['posVer']) + 'px;'       \
-      + ' width: ' + str(i.width) + 'px;'                                                            \
-      + ' height: ' + str(i.height) + 'px;'                                                          \
+    cssString += subimage['cssSelector'] + pseudo + ' {'                                                \
+      + ' background-position: -' + str(subimage['posHor']) + 'px -' + str(subimage['posVer']) + 'px;'  \
+      + ' width: ' + str(i.width) + 'px;'                                                               \
+      + ' height: ' + str(i.height) + 'px;'                                                             \
       + ' }\n'
     
     if cssAllClasses != '':
       cssAllClasses += ',\n'
-    cssAllClasses += iconKey + pseudo
+    cssAllClasses += subimage['cssSelector'] + pseudo
 
     index = index + 1
   
-  cssAdds = json_db.get('cssAdds')
-  if cssAdds is not None:
+  cssCommon = json_db.get('cssCommon')
+  if cssCommon is not None:
     cssAllClasses += '{\n'
-    for s in cssAdds:
+    for s in cssCommon:
       cssAllClasses += '  ' + s + ';\n'
     cssAllClasses += '}\n'
     cssString += '\n' + cssAllClasses
 
-  spriteOutputBaseName = getFullFilename(json_db['spriteOutputBaseName'], rootDirIcons)
-  png_result = spriteOutputBaseName + '.png'
+  spriteFilename = getFullFilename(json_db['spriteFilename'], rootDirIcons)
+  png_result = spriteFilename + '.png'
   print('Save ' +  png_result)
   sprite.save(png_result, optimize=True)
   if (shutil.which('optipng') is not None):
@@ -100,12 +92,12 @@ def create_sprites(spriteJsonFilename):
   # save as webp
   # https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#webp
   # method=6 provides a better size, but is slow
-  webp_result = spriteOutputBaseName + '.webp'
+  webp_result = spriteFilename + '.webp'
   print('Save ' +  webp_result)
   sprite.save(webp_result, method=6, quality=100, lossless=True)
 
   # save css file, or print on the console
-  cssFilename = json_db.get('cssOutputFilename')
+  cssFilename = json_db.get('cssFilename')
   if cssFilename is None:
     print('\n=======================  copy/paste the sprite position in your favorite css file')
     print(cssString)
@@ -120,7 +112,7 @@ def create_sprites(spriteJsonFilename):
 
 
 # utility function to get the full filename given a filename (absolute or relative) and the
-# root directory of the sprite json description file
+# root directory of the sprite json desription file
 def getFullFilename(filename, root):
   if (os.path.isabs(filename)):
     return filename
