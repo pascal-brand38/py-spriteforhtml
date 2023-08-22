cc7f17380413d0e2f70c6b1e0d5d24bf4d7ce473
Pascal Brand <pascal.brand38@gmail.com>
2023-08-22 17:12:32 +0200

    code refactoring
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

--- src/spriteforhtml/create.py
+++ src/spriteforhtml/create.py
@@ -3,13 +3,12 @@
 # Copyright (c) 2023 Pascal Brand
 
 from PIL import Image
+from ._sort import sortSubimages
 
 import os
 import json
 import shutil
-import functools
 
-# TODO: strategy: square, hor, ver, auto (hor or ver, depending on the max size of images)
 # TODO: automatic css named class, with or without prefix
 # TODO: through command-line only, without json
 
@@ -50,23 +49,6 @@ def _checkJson(json_db):
   if strategy is not None:
     if (strategy!='auto') and (strategy!='hor') and (strategy!='ver') and (strategy!='square'):
       _error('Error in spriteforhtml.create.create_sprites: "strategy" must be "auto", "hor", "ver" or "square"')
-
-
-def _setStrategy(json_db):
-  if json_db.get('strategy') is None:
-    json_db['strategy'] = 'auto'
-  if json_db['strategy'] == 'auto':
-    w = -1
-    h = -1
-    for subimage in json_db['subimages']:
-      w1, h1 = subimage['pil'].size
-      w = max(w, w1)
-      h = max(h, h1)
-      if (w < h):
-        json_db['strategy'] = 'hor'
-      else:
-        json_db['strategy'] = 'ver'
-
 
 
 # Open sub images, and add them in the json_db to further usage
@@ -117,63 +99,6 @@ def _checkOverlapping(subimages):
   return False, ''
 
 
-# sort the subimages such that
-# - the one that have posHor have the same order
-# - the bigger ones are positionned first
-def _compare(i1, i2):
-  if i1.get('posHor') is not None:
-    if i2.get('posHor') is not None:
-      return 0    # i1 and i2 can be interchanged
-    else:
-      return -1   # i1 is before i2
-
-  if i2.get('posHor') is not None:
-    return +1   # i2 is before i1
-
-  w1, h1 = i1['pil'].size
-  w2, h2 = i2['pil'].size
-  return None, w1, h1, w2, h2
-  # return w2*h2 - w1*h1
-
-def _compareHor(i1, i2):
-  r, w1, h1, w2, h2 = _compare(i1, i2)
-  if r is not None:
-    return r
-  if (h1 > h2):
-    return -1
-  if (h1 < h2):
-    return +1
-  return w2 - w1
-  # return w2*h2 - w1*h1
-
-def _compareVer(i1, i2):
-  r, w1, h1, w2, h2 = _compare(i1, i2)
-  if r is not None:
-    return r
-  if (w1 > w2):
-    return -1
-  if (w1 < w2):
-    return +1
-  return h2 - h1
-  # return w2*h2 - w1*h1
-
-def _compareSquare(i1, i2):
-  r, w1, h1, w2, h2 = _compare(i1, i2)
-  if r is not None:
-    return r
-  return w2*h2 - w1*h1
-
-# Calling
-def _sortSubimages(json_db):
-  cmp = None
-  match json_db['strategy']:
-    case 'hor':
-      cmp = _compareHor
-    case 'ver':
-      cmp = _compareVer
-    case 'square':
-      cmp = _compareSquare
-  json_db['subimages'].sort(key=functools.cmp_to_key(cmp))
 
 def _spriteSize(subimages):
   sprite_width = 0
@@ -256,32 +181,21 @@ def create_sprites(spriteJsonFilename):
   if error:
     _error(msg)
 
-  _setStrategy(json_db)
-  _sortSubimages(json_db)
-
-  cssString = '/* Generated using python package spriteforhtml */\n\n'
-  cssAllClasses = ''
-
-  sprite_width = 0
-  sprite_height = 0
+  sortSubimages(json_db)
 
   for subimage in json_db['subimages']:
     if subimage.get('posHor') is None:
       _placeSubimage(json_db['subimages'], subimage, json_db['strategy'])
-    pos_w = int(subimage['posHor'])
-    pos_h = int(subimage['posVer'])
-    w = subimage['pil'].width
-    h = subimage['pil'].height
-    if sprite_width < pos_w + w:
-      sprite_width = pos_w + w
-    if sprite_height < pos_h + h:
-      sprite_height = pos_h + h
+  
+  sprite_width, sprite_height = _spriteSize(json_db['subimages'])
 
   sprite = Image.new(
     mode='RGBA',
     size=(sprite_width, sprite_height),
     color=(0,0,0,0))  # fully transparent
 
+  cssString = '/* Generated using python package spriteforhtml */\n\n'
+  cssAllClasses = ''
   for subimage in json_db['subimages']:
     i = subimage['pil']
     pos_w = int(subimage['posHor'])
